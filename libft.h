702af4a2ac37ca1aa5097544120cc4a522d06252
0xNino <86327707+0xNino@users.noreply.github.com>
2021-10-26 12:08:48 +0200

    initialize makefile+libft.h and correct functions

--- libft.h
+++ libft.h
@@ -1,18 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: 0xNino <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/26 11:16:36 by 0xNino           #+#    #+#             */
-/*   Updated: 2021/10/26 11:16:40 by 0xNino          ###   ########.fr       */
+/*   Created: 2021/10/26 11:32:34 by 0xNino           #+#    #+#             */
+/*   Updated: 2021/10/26 11:32:37 by 0xNino          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include	"libft.h"
+#ifndef LIBFT_H
+# define LIBFT_H
 
-int	ft_isalnum(unsigned char c)
-{
-	return ((ft_isalpha(c) || ft_isdigit(c)));
-}
+# include <unistd.h>
+# include <stdio.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+
+#endif
